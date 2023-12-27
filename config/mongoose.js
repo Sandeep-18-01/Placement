4c0a8ed715eafb5e2cb9212ae68da03d7fe58be7
Sai Kiran <bhaskarla-sandeep.sai-kiran@capgemini.com>
2023-12-27 14:41:04 +0530

    first commit

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,13 +1,19 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/cell");
+// Set the strictQuery option to avoid the deprecation warning
+mongoose.set("strictQuery", false);
+
+mongoose.connect("mongodb://localhost:27017/cell", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 const db = mongoose.connection;
 
 db.on("error", console.error.bind(console, "Error in database connection!!!"));
 
 db.once("open", () => {
-  console.log("Connection to database is succesfull.");
+  console.log("Connection to the database is successful.");
 });
 
 module.exports = db;
